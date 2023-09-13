5999cf55a875db7e47b011f84065ec63d9c8670f
kim3722 <kim3722@purdue.edu>
2023-09-13 00:30:25 -0400

    lab3

---
+++ lab3/f2022-lab-7-main/src/main.c
@@ -0,0 +1,192 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Weili An
+  * @version V1.0
+  * @date    Oct 24, 2022
+  * @brief   ECE 362 Lab 7 template
+  ******************************************************************************
+*/
+
+
+#include "stm32f0xx.h"
+#include <stdint.h>
+
+// Global data structure
+char* login          = "xyz"; // Replace with your login.
+char disp[9]         = "Hello...";
+uint8_t col          = 0;
+uint8_t mode         = 'A';
+uint8_t thrust       = 0;
+int16_t fuel         = 800;
+int16_t alt          = 4500;
+int16_t velo         = 0;
+
+// Make them visible to autotest.o
+extern char* login;
+// Keymap is in `font.S` to match up what autotester expected
+extern char keymap;
+extern char disp[9];
+extern uint8_t col;
+extern uint8_t mode;
+extern uint8_t thrust;
+extern int16_t fuel;
+extern int16_t alt;
+extern int16_t velo;
+
+char* keymap_arr = &keymap;
+
+// Font array in assembly file
+// as I am too lazy to convert it into C array
+extern uint8_t font[];
+
+// The functions we should implement
+void enable_ports();
+void setup_tim6();
+void show_char(int n, char c);
+void drive_column(int c);
+int read_rows();
+char rows_to_key(int rows);
+void handle_key(char key);
+void setup_tim7();
+void write_display();
+void update_variables();
+void setup_tim14();
+
+// Auotest functions
+extern void check_wiring();
+extern void autotest();
+extern void fill_alpha();
+
+int main(void) {
+    // check_wiring();
+    autotest();
+    // fill_alpha();
+    enable_ports();
+    setup_tim6();
+    setup_tim7();
+    setup_tim14();
+
+    for(;;) {
+        asm("wfi");
+    }
+}
+
+/**
+ * @brief Enable the ports and configure pins as described
+ *        in lab handout
+ * 
+ */
+void enable_ports(){
+
+}
+
+//-------------------------------
+// Timer 6 ISR goes here
+//-------------------------------
+// TODO
+
+
+/**
+ * @brief Set up timer 6 as described in handout
+ * 
+ */
+void setup_tim6() {
+
+}
+
+/**
+ * @brief Show a character `c` on column `n`
+ *        of the segment LED display
+ * 
+ * @param n 
+ * @param c 
+ */
+void show_char(int n, char c) {
+
+}
+
+/**
+ * @brief Drive the column pins of the keypad
+ *        First clear the keypad column output
+ *        Then drive the column represented by `c`
+ * 
+ * @param c 
+ */
+void drive_column(int c) {
+
+}
+
+/**
+ * @brief Read the rows value of the keypad
+ * 
+ * @return int 
+ */
+int read_rows() {
+
+}
+
+/**
+ * @brief Convert the pressed key to character
+ *        Use the rows value and the current `col`
+ *        being scanning to compute an offset into
+ *        the character map array
+ * 
+ * @param rows 
+ * @return char 
+ */
+char rows_to_key(int rows) {
+
+}
+
+/**
+ * @brief Handle key pressed in the game
+ * 
+ * @param key 
+ */
+void handle_key(char key) {
+
+}
+
+//-------------------------------
+// Timer 7 ISR goes here
+//-------------------------------
+// TODO
+
+/**
+ * @brief Setup timer 7 as described in lab handout
+ * 
+ */
+void setup_tim7() {
+
+}
+
+/**
+ * @brief Write the display based on game's mode
+ * 
+ */
+void write_display() {
+
+}
+
+/**
+ * @brief Game logic
+ * 
+ */
+void update_variables() {
+
+}
+
+//-------------------------------
+// Timer 14 ISR goes here
+//-------------------------------
+// TODO
+
+/**
+ * @brief Setup timer 14 as described in lab
+ *        handout
+ * 
+ */
+void setup_tim14() {
+
+}
