fc42c4bc57ccd7c2836d0c255a48d40c84db9cef
kim3722 <kim3722@purdue.edu>
2023-09-13 00:31:53 -0400

    lab3

--- lab3/f2022-lab-7-main/src/main.c
+++ lab3/f2022-lab-7-main/src/main.c
@@ -78,6 +78,17 @@ int main(void) {
  * 
  */
 void enable_ports(){
+    RCC -> AHBENR |= RCC_AHBENR_GPIOBEN;
+    GPIOB -> MODER &= 0xffc00000;
+    GPIOB -> MODER |= 0x00155555;
+
+    RCC -> AHBENR |= RCC_AHBENR_GPIOCEN;
+    GPIOC -> MODER &= 0xfffc00ff;
+    GPIOC -> MODER |= 0x00015500; //output
+    GPIOC -> MODER &= 0xffffff00;
+
+    GPIOC -> PUPDR &= 0xffffff00;
+    GPIOC -> PUPDR |= 0x000000aa;
 
 }
 
@@ -85,6 +96,15 @@ void enable_ports(){
 // Timer 6 ISR goes here
 //-------------------------------
 // TODO
+void TIM6_DAC_IRQHandler() {
+    TIM6 -> SR &= ~TIM_SR_UIF;
+    if ((GPIOC -> ODR >> 8 & 1) == 1) {
+        GPIOC -> BRR = 0x0100;
+    }
+    else {
+        GPIOC -> BSRR = 0x0100;
+    }
+}
 
 
 /**
@@ -92,7 +112,12 @@ void enable_ports(){
  * 
  */
 void setup_tim6() {
-
+    RCC -> APB1ENR |= RCC_APB1ENR_TIM6EN;
+    TIM6 -> PSC = 48000 - 1;
+    TIM6 -> ARR = 500 - 1;
+    TIM6 -> DIER |= TIM_DIER_UIE;
+    TIM6 -> CR1 |= TIM_CR1_CEN;
+    NVIC -> ISER[0] = 1 << TIM6_DAC_IRQn;
 }
 
 /**
@@ -103,6 +128,18 @@ void setup_tim6() {
  * @param c 
  */
 void show_char(int n, char c) {
+//    make sure 0 <= n <= 7, i.e. lies in the valid range
+//    assert(0 <= n || n <= 7);
+    if (!(0 <= n && n <= 7)) {
+        return;
+    }
+   // font[c] contains the bit setting for PB0-7 (the sink driver)
+//    GPIOB -> ODR |= hex_val;
+//        write content of font[c] to the corresponding bits in `GPIOB->ODR` (i.e. those pins controlling the decoder and the sink driver)
+    int sum = (n * 16) << 4 | font[c];
+    GPIOB -> ODR &= 0xfffff000;
+    GPIOB -> ODR |= sum;
+
 
 }
 
@@ -114,7 +151,12 @@ void show_char(int n, char c) {
  * @param c 
  */
 void drive_column(int c) {
-
+//    c = least 2 bits of c
+    int two_bit = c & 0b11;
+//    first clear the bits 4-7 of GPIOC
+    GPIOC -> BRR = 0x00f0;
+//    then set the bits corresponding to the column `c`
+    GPIOC -> BSRR = 1 << 4 + two_bit;
 }
 
 /**
@@ -123,7 +165,7 @@ void drive_column(int c) {
  * @return int 
  */
 int read_rows() {
-
+    return GPIOC -> IDR & 0xf;
 }
 
 /**
@@ -136,7 +178,21 @@ int read_rows() {
  * @return char 
  */
 char rows_to_key(int rows) {
-
+//    compute the offset of the button being pressed right now from `rows` and `col`
+//    Note `rows` will be a 4bit value from reading the IDR register of the row pins of the keypad
+    int off_set = col * 4;
+    if (rows == 1){
+        return keymap_arr[off_set];
+    }
+    else if (rows == 2) {
+        return keymap_arr[off_set + 1];
+    }
+    else if (rows == 4) {
+        return keymap_arr[off_set + 2];
+    }
+    else if (rows == 8) {
+        return keymap_arr[off_set + 3];
+    }
 }
 
 /**
@@ -145,19 +201,48 @@ char rows_to_key(int rows) {
  * @param key 
  */
 void handle_key(char key) {
-
+    if (key == 'A' || key == 'B' || key == 'D') {
+//        set mode to key
+        mode = key;
+    }
+    else { // if key is a digit
+//        set thrust to the represented value of key, i.e. if key == '1', thrust = 1, not '1'
+        thrust = key - 48;
+    }
 }
 
 //-------------------------------
 // Timer 7 ISR goes here
 //-------------------------------
 // TODO
+void TIM7_IRQHandler() {
+    TIM7 -> SR &= ~TIM_SR_UIF;
+    int rows = read_rows();
+
+    if (rows != 0) {
+        int key = rows_to_key(rows);
+        handle_key(key);
+    }
+
+    show_char(col, disp[col]);
+    col += 1;
+    if (col > 7) {
+        col = 0;
+    }
+    drive_column(col);
+}
 
 /**
  * @brief Setup timer 7 as described in lab handout
  * 
  */
 void setup_tim7() {
+    RCC -> APB1ENR |= RCC_APB1ENR_TIM7EN;
+    TIM7 -> PSC = 24000 - 1;
+    TIM7 -> ARR = 2 - 1;
+    TIM7 -> DIER |= TIM_DIER_UIE;
+    TIM7 -> CR1 |= TIM_CR1_CEN;
+    NVIC -> ISER[0] = 1 << TIM7_IRQn;
 
 }
 
